2c5310b18b613805cdc9c3a78f1c41c05a02bf7a
Tirth Void <tirth11804@gmail.com>
2026-08-30 01:54:50 -0400

    did some work with the tcp listener and got it to parse ips. ill work on trying to actually hash the ip and decide what server to go from there

--- seesaw/src/main.rs
+++ seesaw/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::net::{
+    TcpListener,
+    TcpStream,
+    IpAddr,
+    // Ipv4Addr,
+    SocketAddrV4,
+};
+
+//TODO: implement all the cool little different load balancing decision thingies
+
+//TcpStream::connect(socket: String);
+
+fn redirect(stream: TcpStream, server_sockets: &Vec<SocketAddrV4>) -> Result<(), Box<dyn Error>> {
+    let ip: IpAddr = stream.peer_addr()?.ip();
+    let n = server_sockets.len();
+    // let ip_int: u32 = u32::from(ip);
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    //TODO: make host and port read from config.toml
+    let host = "127.0.0.1";
+    let port = "8000";
+
+    let listener = TcpListener::bind(format!("{}:{}", host, port))?;
+    //TODO: (maybe) make server_sockets read from config.toml & encrypt them so they arent in plaintext
+    let server_sockets: Vec<SocketAddrV4> = vec![];
+
+    for stream in listener.incoming() {
+        let _ = redirect(stream?, &server_sockets);
+    }
+
+    Ok(())
 }
